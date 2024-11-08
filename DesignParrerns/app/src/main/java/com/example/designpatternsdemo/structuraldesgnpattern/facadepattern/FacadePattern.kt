3b13dea3219d684230d136bc7a0622f863e40d2d
Raj Kumar Singh <rksinghj4@gmail.com>
2024-11-08 20:27:41 +0530

    Added facade pattern

---
+++ DesignParrerns/app/src/main/java/com/example/designpatternsdemo/structuraldesgnpattern/facadepattern/FacadePattern.kt
@@ -0,0 +1,90 @@
+package com.example.designpatternsdemo.structuraldesgnpattern.facadepattern
+
+/**
+ * When to use: To hide system complexity from the client.
+ *It provides a simplified interface to a complex system, making it easier to use.
+ */
+
+//Complex subsystem or some 3rd part library or data storage
+class CollegeLibrary {
+    //It's not a book library. It act as a 3rd party library.
+    // Which supports all required functionality in college db.
+    class DepartmentDao {
+        fun insertDepartment() {
+            println("Department details are inserted")
+        }
+
+        fun getDepartment() {
+            println("Department details are fetched")
+        }
+
+        fun updateDepartment() {
+            println("Department details are updated")
+        }
+
+        fun deleteDepartment() {
+            println("Department details are deleted")
+        }
+    }
+
+    class EmployeeDao {
+        fun insertEmployee() {
+            println("Employee details are inserted")
+        }
+
+        fun getEmployee() {
+            println("Employee details are fetched")
+        }
+
+        fun updateEmployee() {
+            println("Employee details are updated")
+        }
+
+        fun deleteEmployee() {
+            println("Employee details are deleted")
+        }
+    }
+
+    //It might have #n of different methods.
+}
+
+/**
+ * A facade class that serves as the middle layer between the client and the subsystems.
+ * This class should offer simplified methods that wrap the interactions with the subsystems.
+ *
+ * Benefits: Expose only what is needed to client. Hide everything else.
+ */
+class EmployeeFacade {
+    private val employeeDao = CollegeLibrary.EmployeeDao()
+    fun insertEmployee() {
+        /**
+         *  Inside the facade methods, delegate the requests to the appropriate subsystem classes
+         *  to perform the actual operations.
+         */
+        employeeDao.insertEmployee()
+    }
+
+    fun getEmployee() {
+        //Tomorrow if return type is changes then just Facade need get updated.
+        employeeDao.getEmployee()
+    }
+
+    fun updateEmployee() {
+        employeeDao.updateEmployee()
+    }
+
+    fun deleteEmployee() {
+        employeeDao.deleteEmployee()
+    }
+}
+
+
+//Client
+private fun main() {
+    val employeeFacade = EmployeeFacade()
+    employeeFacade.insertEmployee()
+    employeeFacade.updateEmployee()
+    employeeFacade.getEmployee()
+    employeeFacade.deleteEmployee()
+}
+
---
+++ DesignParrerns/app/src/main/java/com/example/designpatternsdemo/structuraldesgnpattern/facadepattern/FacadePattern2.kt
@@ -0,0 +1,63 @@
+package com.example.designpatternsdemo.structuraldesgnpattern.facadepattern
+
+data class Product(val prodId: Int, val name: String, val desc: String, val price: Float)
+class AmazonDao {
+    private val listOfProduct = mutableListOf(
+        Product(1, "Pen", "Black", 100f),
+        Product(2, "Stylus", "Black", 120f),
+        Product(3, "Remarkable", "White", 130f),
+        Product(4, "Kobo", "White", 140f),
+    )
+
+    fun getProduct(prodId: Int): Product? = listOfProduct.firstOrNull { it.prodId == prodId }
+
+}
+
+class Payment {
+    fun makePayment(price: Float) {
+        println("Payment done of Rs. $price/-")
+    }
+}
+
+class Invoice {
+    fun generateInvoice(product: Product) {
+        println("Invoice generated for product:  $product")
+    }
+}
+
+class Notification {
+    fun sendNotification(product: Product) {
+        println("Notification is sent to customer mobile for product: $product")
+    }
+}
+
+//Facade - a simplified interface to a complex system, making it easier to use.
+class OrderFacade {
+    private val amazonDao = AmazonDao()
+    private val payment = Payment()
+    private val invoice = Invoice()
+    private val notification = Notification()
+
+    //Facade is responsible for handling all the complexity behind the order placement.
+    fun placeOrder(prodId: Int) {
+        //Sequence of method calls matters here, but client is free from all responsibilities.
+        //Tomorrow if new step is introduce then just Facade will change but not the entire client code
+        //Tomorrow if one method's return type is changed then just facade will changes
+        // and convert the new return type to what client is already using.
+        val product = amazonDao.getProduct(prodId)
+        if (product == null) {
+            println("Product not found")
+        } else {
+            println("$product is found")
+            payment.makePayment(product.price)
+            invoice.generateInvoice(product)
+            notification.sendNotification(product)
+        }
+    }
+}
+
+//Client
+private fun main() {
+    val orderFacade = OrderFacade()
+    orderFacade.placeOrder(2)
+}
